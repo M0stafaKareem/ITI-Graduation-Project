da6d7ca0ab76a3837d8da4406cc2f4cae1a35457
M0stafaKareem <93432503+M0stafaKareem@users.noreply.github.com>
2024-09-19 00:21:02 +0300

    login + Registeration Functionalities

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginFromComponent } from './login-from/login-from.component';
+import { RegisterFromComponent } from './register-from/register-from.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'register', component: RegisterFromComponent, title: 'Register' },
+  { path: '', component: LoginFromComponent, title: 'Login' },
+];
---
+++ frontend/src/app/login-from/login-from.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { LoginService } from './login.service';
+import { FormControl, FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login-from',
+  standalone: true,
+  imports: [RouterLink, FormsModule],
+  templateUrl: './login-from.component.html',
+  styleUrls: [
+    './login-from.component.css',
+    'fonts/material-design-iconic-font/css/material-design-iconic-font.min.css',
+  ],
+})
+export class LoginFromComponent {
+  enteredEmail: string = '';
+  enteredPassword: string = '';
+
+  constructor(private loginService: LoginService) {}
+  async onLoginHandler() {
+    if (
+      await this.loginService.verifyCredentials(
+        this.enteredEmail,
+        this.enteredPassword
+      )
+    ) {
+      console.log('Login successful');
+    } else {
+      console.log('Invalid credentials');
+    }
+  }
+}
