07b380d537dd60229f1f8df19f4b8cc4bc3b86ff
Marwan Moataz <159698698+marwan-moataz@users.noreply.github.com>
2024-09-20 21:30:25 +0300

    clients & cases lists + delete functionality + responsive table

--- frontend/src/app/components/cases/cases.component.ts
+++ frontend/src/app/components/cases/cases.component.ts
@@ -1,13 +1,136 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { CaseComponent } from './case/case.component';
 import { TableComponent } from '../../shared/table/table.component';
 import { SecondaryNavComponent } from '../../shared/secondary-nav/secondary-nav.component';
+import { CasesService } from '../../shared/services/cases.service';
+import { NgFor, NgIf } from '@angular/common';
+import { ClientsService } from '../../shared/services/clients.service';
+import { ClientComponent } from '../clients/client/client.component';
+import { Case } from '../../shared/models/case.model';
+import { CaseCategory } from '../../shared/models/case.category.model';
+import { CaseGrade } from '../../shared/models/case.grade.model';
+import { Clients } from '../../shared/models/clients.model';
 
 @Component({
   selector: 'app-cases',
   standalone: true,
-  imports: [CaseComponent, TableComponent, SecondaryNavComponent],
+  imports: [
+    CaseComponent,
+    TableComponent,
+    SecondaryNavComponent,
+    NgFor,
+    ClientComponent,
+    NgIf,
+  ],
   templateUrl: './cases.component.html',
   styleUrl: './cases.component.css',
 })
-export class CasesComponent {}
+export class CasesComponent {
+  cases?: Array<Case>;
+  categories?: Array<CaseCategory>;
+  grades?: Array<CaseGrade>;
+  clients?: Array<Clients>;
+  client!: Clients;
+  loading: boolean = false;
+  constructor(
+    private caseService: CasesService,
+    private clientService: ClientsService
+  ) {}
+
+  ngOnInit() {
+    this.loadCases();
+    this.loadCategories();
+    this.getCaseGrade();
+    this.getClient();
+  }
+
+  loadCases(): void {
+    this.caseService.getCases().subscribe({
+      next: (data) => {
+        this.cases = data;
+      },
+      error: (error) => console.error('Error:', error),
+    });
+  }
+
+  loadCategories(): void {
+    this.caseService.getCategories().subscribe({
+      next: (categoriesData) => {
+        this.categories = categoriesData;
+        this.cases = this.cases?.map((caseItem: Case) => ({
+          ...caseItem,
+          categoryName:
+            this.categories?.find(
+              (cat: CaseCategory) => cat.id === caseItem.case_category_id
+            )?.name || 'No Category',
+        }));
+      },
+      error: (error) => console.error('Error:', error),
+    });
+  }
+
+  getCaseGrade(): void {
+    this.caseService.getCaseGrade().subscribe({
+      next: (gradeData) => {
+        this.grades = gradeData;
+        this.cases = this.cases?.map((caseItem: Case) => ({
+          ...caseItem,
+          case_grade:
+            this.grades?.find(
+              (grade: CaseGrade) => grade.id === caseItem.case_grade_id
+            )?.name || 'No Grade',
+        }));
+      },
+      error: (error) => console.error('Error:', error),
+    });
+  }
+
+  getClient(): void {
+    this.clientService.getClients().subscribe({
+      next: (clientsData) => {
+        this.clients = clientsData;
+        this.cases = this.cases?.map((caseItem: Case) => {
+          const foundClient = this.clients?.find(
+            (client: Clients) => client.id === caseItem.client_id
+          );
+          console.log(foundClient);
+
+          return {
+            ...caseItem,
+            client: foundClient,
+          };
+        });
+
+        console.log(this.cases);
+      },
+      error: (error) => console.error('Error:', error),
+    });
+  }
+
+  onActionSelect(event: any, caseId: number): void {
+    const selectedValue = event.target.value;
+
+    if (selectedValue === 'Delete') {
+      this.deleteCase(caseId);
+    }
+  }
+
+  deleteCase(caseId: number): void {
+    if (confirm('Are you sure you want to delete this case?')) {
+      this.loading = true;
+      this.caseService.deleteCase(caseId).subscribe({
+        next: () => {
+          this.cases = this.cases?.filter(
+            (caseItem: Case) => caseItem.id !== caseId
+          );
+
+          this.loading = false;
+        },
+        error: (error) => {
+          console.error('Error deleting case:', error);
+          this.loading = false;
+        },
+      });
+    }
+  }
+}
--- frontend/src/app/components/clients/client/client.component.ts
+++ frontend/src/app/components/clients/client/client.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-client',
+  selector: 'td [client-td]',
   standalone: true,
   imports: [],
   templateUrl: './client.component.html',
-  styleUrl: './client.component.css'
+  styleUrls: ['./client.component.css', '../../cases/case/case.component.css'],
 })
 export class ClientComponent {
-
+  @Input() clientName?: string;
+  @Input() clientMobile: any;
+  @Input() clientEmail: any;
+  @Input() clientAddress: any;
 }
