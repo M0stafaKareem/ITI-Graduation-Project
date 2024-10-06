893b70fd89ad6fc21ece3e26c78a82a1f29f89ee
polaadelovic99 <polaadelovic99@gmail.com>
2024-10-07 01:26:43 +0300

    FAQs Section intialization

--- landingPage/src/app/app.component.ts
+++ landingPage/src/app/app.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SliderComponent } from './slider/slider.component';
-// import { ServicesComponent } from "./services/services.component";
 import { StatsComponent } from "./stats/stats.component";
 import { AboutusComponent } from "./aboutus/aboutus.component";
-import { ServicesComponent } from "./services/services.component";  
+import { ServicesComponent } from "./services/services.component";
+import { FaqsComponent } from "./faqs/faqs.component";  
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SliderComponent, StatsComponent, AboutusComponent, ServicesComponent],
+  imports: [RouterOutlet, SliderComponent, StatsComponent, AboutusComponent, ServicesComponent, FaqsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
