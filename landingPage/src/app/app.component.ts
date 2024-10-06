70ce8a93ac5aa1795b3d5e0749f2a6af8a1d1f23
polaadelovic99 <polaadelovic99@gmail.com>
2024-10-07 01:16:22 +0300

    Services Section

--- landingPage/src/app/app.component.ts
+++ landingPage/src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { SliderComponent } from './slider/slider.component';
 // import { ServicesComponent } from "./services/services.component";
 import { StatsComponent } from "./stats/stats.component";
-import { AboutusComponent } from "./aboutus/aboutus.component";  
+import { AboutusComponent } from "./aboutus/aboutus.component";
+import { ServicesComponent } from "./services/services.component";  
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SliderComponent, StatsComponent, AboutusComponent],
+  imports: [RouterOutlet, SliderComponent, StatsComponent, AboutusComponent, ServicesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
