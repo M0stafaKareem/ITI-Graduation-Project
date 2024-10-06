7c0be78faa3e8076f1e3a50242f26b61c75b882e
polaadelovic99 <polaadelovic99@gmail.com>
2024-10-07 01:59:46 +0300

    add footer to the landing page

--- landingPage/src/app/app.component.ts
+++ landingPage/src/app/app.component.ts
@@ -6,12 +6,13 @@ import { AboutusComponent } from "./aboutus/aboutus.component";
 import { ServicesComponent } from "./services/services.component";
 import { FaqsComponent } from "./faqs/faqs.component";
 import { ContactComponent } from "./contact/contact.component";  
+import { FooterComponent } from './footer/footer.component'; 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SliderComponent, StatsComponent, AboutusComponent, ServicesComponent, FaqsComponent, ContactComponent],
+  imports: [RouterOutlet, SliderComponent, StatsComponent, AboutusComponent, ServicesComponent, FaqsComponent, ContactComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
