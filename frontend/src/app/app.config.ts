48d582787df543ee2c301601121d304d7e923c29
Marwan Moataz <159698698+marwan-moataz@users.noreply.github.com>
2024-10-07 18:19:08 +0300

    add pagination to case-list

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -4,6 +4,8 @@ import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,5 +13,7 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     provideAnimations(),
     provideToastr(),
+    MatPaginatorModule,
+    NgxPaginationModule,
   ],
 };
